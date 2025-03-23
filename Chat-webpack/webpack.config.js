5cf035aa93069bb29b12cb9e72e51a9be9c114ba
Melquizedeque S. Lobo <melquizedeque.s.lobo@gmail.com>
2025-03-23 18:32:34 -0300

    refatoração da configuração do Webpack e remoção do arquivo compress

--- Chat-webpack/webpack.config.js
+++ Chat-webpack/webpack.config.js
@@ -7,57 +7,57 @@ const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js', // Temporário, será embutido
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'], // Extrai o CSS temporariamente
-      },
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js', // Temporário, será embutido
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'], // Extrai o CSS temporariamente
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                minifyCSS: true,
+                minifyJS: true,
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css', // Arquivo temporário de CSS
+        }),
+        new HtmlInlineCssWebpackPlugin(), // Embuti o CSS extraído no HTML
+        new HtmlInlineScriptPlugin(), // Embuti o JS inline
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        minifyCSS: true,
-        minifyJS: true,
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css', // Arquivo temporário de CSS
-    }),
-    new HtmlInlineCssWebpackPlugin(), // Embuti o CSS extraído no HTML
-    new HtmlInlineScriptPlugin(), // Embuti o JS inline
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true, // Opcional
-            pure_funcs: ['console.info', 'console.debug'],
-          },
-          mangle: true,
-          output: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-      new CssMinimizerPlugin(), // Minifica o CSS antes de embutir
-    ],
-  },
-  mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: true, // Opcional
+                        pure_funcs: ['console.info', 'console.debug'],
+                    },
+                    mangle: true,
+                    output: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+            new CssMinimizerPlugin(), // Minifica o CSS antes de embutir
+        ],
+    },
+    mode: 'production',
 };
